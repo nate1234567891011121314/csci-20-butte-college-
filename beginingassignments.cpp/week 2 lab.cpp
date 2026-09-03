f0b56860e12707db0985e00b0660567fa8aea3a0
nate1234567891011121314 <nateyahuaca@gmail.com>
2026-09-03 20:40:11 +0000

    week 3 assignment explain hellow world

---
+++ beginingassignments.cpp/week 2 lab.cpp
@@ -0,0 +1,19 @@
+// this is storaging information 
+// always in this format <libaray name> why is there different libarays i dont know yet. always first thing c++ interacts
+#include<iostream>
+
+/*
+/say what i would like to include from a libaray which calls specific objects from liabray/ill be using std which means its in standard
+.double collen :: means inside of standard/while semi collen ; means full stop of a statement(which has to make sense)
+ using (we will use object from libaray)/std standard(specific book)ish/::(in this spefic place)/cout(acomplishes language tasks output text)/;fullstop
+*/
+
+using std::cout;
+
+//entry point(where the logic begins to start for goal) aka main
+int main() { 
+
+    return 0;
+}
+//outputs integer int/starts logic main/ what is being input ()/ defines what main is {}/ only and always brings 0/full stop ;
+// int is needed to figure out sucess or fail
